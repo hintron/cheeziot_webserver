486b4f3e7616eafb19b7dfd80984cc8301837ede
Michael Hinton <michael.g.hinton@gmail.com>
2017-04-06 12:33:13 -0600

    Started working with Angular
    Saved off old jQuery implementation as stats-jquery.js.
    Started using Angular, normalize, and bootstrap 4.
    Updated package.json to include angular and normalize.css,
    so it will be easy to obtain the library files.

--- public/stats/js/stats.js
+++ public/stats/js/stats.js
@@ -1,51 +1,58 @@
-$(function(){
+(function () {
+    var app = angular.module("stats", []);
 
-    console.log("Starting polling...");
-    // Start up the polling code
-    start_polling();
+    app.controller('Main', function($scope, $http){
+        console.log("Hello, Angular 1!");
+        console.log("Starting polling...");
+        // Start up the polling code
+        start_polling();
 
-    // Polling code
-    function start_polling(){
-        polling_active = true;
+        // Polling code
+        function start_polling(){
+            polling_active = true;
 
-        // Create a periodic AJAX ping to the server
-        // See http://stackoverflow.com/a/5052661
-        // This will keep running until polling_active is set to false
-        (function poll_worker() {
-            $.ajax({
-                // TODO: Change this url to the /stats endpoint
-                url: '/data',
-                dataType: "json",
-                success: function(json) {
+            // Create a periodic AJAX ping to the server
+            // See http://stackoverflow.com/a/5052661
+            // This will keep running until polling_active is set to false
+            (function poll_worker() {
+                console.log("Poll Worker start");
+                $http.get("/data", {})
+                .then(function (response) {
+                    // Success callback
                     console.log("Success!!!");
+                    console.log(response.data.data);
 
-                    // TODO: Start working with the new stats data, and start updating the HTML page
+                    // TODO: Get statistics data
+                    $scope.records = response.data.data;
 
-                    // TODO: Take code from
-                },
-
-                failure: function(){
-                    console.log("Failure...");
-                    stop_polling();
-                },
-
-                // Note: Complete executes after success does
-                complete: function() {
                     if(polling_active){
                         // Schedule the next request once the current one is complete
                         setTimeout(poll_worker, 5000);
                     }
                 }
-            });
-        })();
-    }
+                , function errorCallback(response) {
+                    // For any errors during the request
+                    console.log("Failure...");
+                    stop_polling();
+                })
+                .catch(function(err){
+                    console.log(err);
+                })
+                .finally(function(){
+                    // Code to execute after either success or failure
+                    console.log("Finally...");
+                });
+            })();
+        }
+
+        // Turns off polling
+        function stop_polling(){
+            polling_active = false;
+        }
 
 
-    // Turns off polling
-    function stop_polling(){
-        polling_active = false;
-    }
+    });
 
 
 
-});
+})();
